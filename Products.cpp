3955fcba2249089ac45e8822537f9852ef5c7497
Shiwali <45665313+pandeyshivali@users.noreply.github.com>
2020-02-03 16:10:58 +0530

    Create Products.cpp

---
+++ Products.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+long long int arr[1000];
+int n,product=1,M=1000000007;
+cin>>n;
+cout<<endl;
+for(int i=1;i<=n;i++){
+    cin>>arr[i];
+}
+for(int i=1;i<=n;i++){
+    product=(product*arr[i])%M;
+}
+cout<<product;
+}
